94c5f5eac36f2247ee253f119aa6608d4df3969a
Sajmul <sajmul1427@gmail.com>
2025-11-24 07:42:01 +0600

    shadcn setup done

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
 const HomePage = () => {
   return (
     <div>
       <h1>This is HomePage component</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
