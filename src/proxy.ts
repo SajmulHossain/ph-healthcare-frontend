c3a250f1eb4a99c244808df4e26952ccc92efa6e
Sajmul <sajmul1427@gmail.com>
2025-11-26 22:06:44 +0600

    different types of route separeted with regex

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,21 +1,47 @@
-import { NextResponse, NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-//   return NextResponse.redirect(new URL('/', request.url))
-console.log("pathname", request.url);
-return NextResponse.next();
+import { NextResponse, NextRequest } from "next/server";
+
+type UserRole = "DOCTOR" | "PATIENT" | "ADMIN";
+
+interface RouteConfig {
+  exact: string[];
+  patterns: RegExp[];
 }
- 
+
+const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+];
+
+const commonProtectedRoutes: RouteConfig = {
+    exact: ["/my-profile", "/settings"],
+    patterns: [],
+}
+
+const doctorProtectedRoutes : RouteConfig = {
+    exact:[],
+    patterns: [/^\/doctor/]
+}
+
+const patientProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\/dashboard/],
+};
+
+const adminProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\/admin/]
+}
+
+export function proxy(request: NextRequest) {
+  //   return NextResponse.redirect(new URL('/', request.url))
+  console.log("pathname", request.url);
+  return NextResponse.next();
+}
+
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
