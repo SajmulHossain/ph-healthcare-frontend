9ae1c04922d2cab648459208f752f738beae541c
Sajmul <sajmul1427@gmail.com>
2025-11-27 18:17:39 +0600

    proxy helpers function creation done

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,5 @@
 import { NextResponse, NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
 
 type UserRole = "DOCTOR" | "PATIENT" | "ADMIN";
 
@@ -15,14 +16,14 @@ const authRoutes = [
 ];
 
 const commonProtectedRoutes: RouteConfig = {
-    exact: ["/my-profile", "/settings"],
-    patterns: [],
-}
+  exact: ["/my-profile", "/settings"],
+  patterns: [],
+};
 
-const doctorProtectedRoutes : RouteConfig = {
-    exact:[],
-    patterns: [/^\/doctor/]
-}
+const doctorProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\/doctor/],
+};
 
 const patientProtectedRoutes: RouteConfig = {
   exact: [],
@@ -31,8 +32,55 @@ const patientProtectedRoutes: RouteConfig = {
 
 const adminProtectedRoutes: RouteConfig = {
   exact: [],
-  patterns: [/^\/admin/]
-}
+  patterns: [/^\/admin/],
+};
+
+const isAuthRoutes = (pathname: string) => {
+  return authRoutes.some((route) => route === pathname);
+};
+
+const isMatchedRoutes = (pathname: string, routes: RouteConfig): boolean => {
+  if (routes.exact.includes(pathname)) {
+    return true;
+  }
+
+  return routes.patterns.some((pattern) => pattern.test(pathname));
+};
+
+const getRouteOwner = (
+  pathname: string
+): "ADMIN" | "DOCTOR" | "PATIENT" | "COMMON" | null => {
+  if (isMatchedRoutes(pathname, adminProtectedRoutes)) {
+    return "ADMIN";
+  }
+  if (isMatchedRoutes(pathname, doctorProtectedRoutes)) {
+    return "DOCTOR";
+  }
+  if (isMatchedRoutes(pathname, patientProtectedRoutes)) {
+    return "PATIENT";
+  }
+  if (isMatchedRoutes(pathname, commonProtectedRoutes)) {
+    return "COMMON";
+  }
+
+  return null;
+};
+
+const getDefaultDashboardRoutes = (role: UserRole): string => {
+  if (role === "ADMIN") {
+    return "/admin/dashboard";
+  }
+
+  if (role === "DOCTOR") {
+    return "/doctor/dashboard";
+  }
+
+  if (role === "PATIENT") {
+    return "/dashboard";
+  }
+
+  return "/";
+};
 
 export function proxy(request: NextRequest) {
   //   return NextResponse.redirect(new URL('/', request.url))
