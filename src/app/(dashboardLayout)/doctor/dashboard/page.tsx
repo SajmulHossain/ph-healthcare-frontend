c871cc969d90a34f3c3da8bc9f92ab16e4708b85
Sajmul <sajmul1427@gmail.com>
2025-11-29 21:54:58 +0600

    table common header done

--- src/app/(dashboardLayout)/doctor/dashboard/page.tsx
+++ src/app/(dashboardLayout)/doctor/dashboard/page.tsx
@@ -1,8 +1,42 @@
+"use client"
+
+import ManagementHeader from "@/components/shared/ManagementHeader";
+import ManagementTable from "@/components/shared/ManagementTable";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
 
 const DoctorDashboardPage = () => {
   return (
     <div>
-    {/* <ManagementTable columns={} /> */}
+      <ManagementHeader
+        title="Management Header"
+        action={{ label: "Read more", onClick: () => {}, icon: MoreHorizontal }}
+      >
+        <Button>A button</Button>
+      </ManagementHeader>
+      <ManagementTable
+        columns={[
+          {
+            header: "Name",
+            accessor: "ki jani ki",
+          },
+          {
+            header: "Email",
+            accessor: "ki jani ki",
+          },
+          {
+            header: "Phone",
+            accessor: () => <Badge>Get Phone</Badge>,
+          },
+        ]}
+        data={[
+          {
+            "ki jani ki": "what",
+          },
+        ]}
+        getRowKey={(key) => key}
+      />
     </div>
   );
 };
